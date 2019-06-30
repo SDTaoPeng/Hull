b263247822e2ca55fcf2f565870655496944fcb3
SDTaoPeng <34082044+SDTaoPeng@users.noreply.github.com>
2019-06-30 08:42:11 +0800

    Add files via upload

---
+++ Segment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ConvexHull_1
+{
+    class Segment
+    {
+        public PointF p;
+        public PointF q;
+
+        public bool contains(PointF point)
+        {
+            if (p.Equals(point) || q.Equals(point))
+
+                return true;
+
+            return false;
+        }
+    }
+}
